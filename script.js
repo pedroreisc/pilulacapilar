32368aa2163abd41cd91d45910afd4d71319cfb1
pedroreisc <129539035+pedroreisc@users.noreply.github.com>
2023-09-29 14:17:06 -0300

    Pilulacapilar
    para o portifólio

---
+++ script.js
@@ -0,0 +1,11 @@
+var btn = document.querySelector('#buing');
+var bloco = document.querySelector('#avs');
+
+btn.addEventListener('click', function(){
+    if(bloco.style.display --- 'block'){
+        bloco.style.display = 'none';
+    } else {
+        bloco.style.display = 'block';
+        
+    }
+})
